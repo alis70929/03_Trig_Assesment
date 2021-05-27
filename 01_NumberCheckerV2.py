54f4faae521a168958e1b7fac6a6673dfb3b6ff7
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-05-27 12:48:42 +1200

    Finished float checker version 2

---
+++ 01_NumberCheckerV2.py
@@ -0,0 +1,32 @@
+# checks if flaot is greater than 0 and less than or equal to upper bound
+def num_check(question, upper_bound=None):
+
+    if upper_bound is not None:
+        error = "please enter a number greater than 0 and lower than or equal to {}".format(upper_bound)
+    else:
+        error = "please enter any number greater than 0"
+
+    valid = False
+    while not valid:
+        try:
+            response = float(input(question))
+
+            if upper_bound is not None:
+                if response > 0 and response < upper_bound:
+                    return response
+                else:
+                    print(error)
+                    continue
+            else:
+                if response > 0:
+                    return response
+                else:
+                    print(error)
+                    continue
+        except ValueError:
+            print(error)
+
+
+side_length = num_check("Side Length: ", None)
+for item in range(0, 3):
+    first_angle = num_check("First angle:", 90)
