51f593344c81a87fd2075910a174e8871246b748
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-06-02 14:19:08 +1200

    Finished Triangle Display

--- 02_TriangleDisplay.py
+++ 02_TriangleDisplay.py
@@ -1,12 +1,16 @@
 def display_triangle():
-    triangle = "V |\ H" \
-               "E |A\ Y" \
-               "R |  \ P"
-T |   \ O
-I |    \ T
-C |     \ E
-A |      \ N
-L |       \ U
-  |        \ S
-  |________B̲\ E
-   HORIZONTAL"
+    triangle = "V |\ H \n" \
+               "E |A\ Y \n" \
+               "R |  \ P \n" \
+               "T |   \ O \n" \
+               "I |    \ T \n" \
+               "C |     \ E \n" \
+               "A |      \ N \n" \
+               "L |       \ U \n" \
+               "  |        \ S \n" \
+               "  |________B\ E \n" \
+               "  HORIZONTAL \n"
+    print(triangle)
+
+
+display_triangle()
