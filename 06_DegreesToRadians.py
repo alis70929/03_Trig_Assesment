166798a9d7319beeae1e45e175bd3cc15d7cc1f9
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-06-08 09:41:27 +1200

    Finished Angle Conversion, Chose V2

--- 06_DegreesToRadians.py
+++ 06_DegreesToRadians.py
@@ -4,11 +4,19 @@ import math
 def angle_conversion(input, to_degrees_or_radians):
 
     if to_degrees_or_radians == "radians":
-        radian = input * ((math.pi) / 180)
+        radian = ((math.pi) / 180) * float(input)
         return radian
 
     if to_degrees_or_radians == "degrees":
-        degrees = input * 180 / math.pi
+        degrees = float(input) * (180 / math.pi)
         return degrees
 
+
 # Main Routine
+for item in range(0, 2):
+    radians = angle_conversion(input("Degrees: "), "radians")
+    print("Radians: {}".format(radians))
+
+for item in range(0, 2):
+    degrees = angle_conversion(input("Radians: "), "degrees")
+    print("Degrees: {}".format(degrees))
---
+++ 06_DegreesToRadiansV2.py
@@ -0,0 +1,10 @@
+import math
+
+# Main Routine
+for item in range(0, 2):
+    radians = math.radians(float(input("Degrees: ")))
+    print("Radians: {}".format(radians))
+
+for item in range(0, 2):
+    degrees = math.degrees(float(input("Radians: ")))
+    print("Degrees: {}".format(degrees))
