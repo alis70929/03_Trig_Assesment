e5cd32ee813d1fed6514807322f1ce3977e3e5d2
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-06-18 12:16:49 +1200

    updated number checker to check if hypotenuse is greater than side length

---
+++ 01_NumberCheckerV3.py
@@ -0,0 +1,40 @@
+# checks if flaot is greater than 0 and less than upper bound
+def num_check(question, upper_bound=None, lower_bound=None):
+
+    if upper_bound is not None:
+        error = "please enter a number greater than 0 and lower than or equal to {}".format(upper_bound)
+    elif lower_bound is not None:
+        error = "please enter a number greater than {}".format(lower_bound)
+    else:
+        error = "please enter any number greater than 0"
+
+    valid = False
+    while not valid:
+        try:
+            response = input(question)
+
+            if response == "":
+                return response
+
+            response = float(response)
+
+            if upper_bound is not None:
+                if response > 0 and response < upper_bound:
+                    return response
+                else:
+                    print(error)
+                    continue
+            else:
+                if response > 0:
+                    return response
+                else:
+                    print(error)
+                    continue
+        except ValueError:
+            print(error)
+
+
+''' for item in range(0, 2):
+    side_length = num_check("Side Length: ", None) '''
+for item in range(0, 4):
+    first_angle = num_check("First angle:", 90)
